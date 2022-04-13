579e4a78e9816bbe778aa1604309fdeaadd8b214
lakshitha191 <75005706+lakshitha191@users.noreply.github.com>
2022-04-13 14:00:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,11 +46,11 @@ function task(i) {
 	
   setTimeout(function() { 
     if(first == true){
-	bot.chat("Subscribe To Easy Gamer : https://www.youtube.com/channel/UCk575VDlrCsv1Gy2XZQ1bSA")
+	bot.chat("Contact LakshiBro :www.Lakshibro.xyz")
 	first = false;
 	}
 	else{
-		bot.chat("Subscribe To MannuG : https://www.youtube.com/channel/UCSZHRnBue50ImBKeR6k6eDg")
+		bot.chat("wassup homies")
 		first = true;
 	}
   }, 3600000 * i); 
